4d45ac0b38b4b1878dccd4cba3f4b86b424bf844
anharfathoni <anharaf28@gmail.com>
2018-11-03 20:33:28 +0700

    update boggle.js

--- boggle.js
+++ boggle.js
@@ -1,11 +1,9 @@
-// var kamus = ['APPLE','SIT','SUPER','TRIP','TURN']
-var kamus = ['TURN']
-
 class Boggle {
     constructor(dim){
         this.dimensi = dim
         this.board = []
         this.kata = kamus
+        this.kataAda = []
     }
 
     generateBoard(){
@@ -33,24 +31,58 @@ class Boggle {
         return this.board
     }
 
-    solve(){
+    cekKamus(){
         console.log("-----------BOARD-----------")
-        // console.log(this.generateBoard())
         console.log(this.generateBoardManual())
+        console.log("kamus yang dicari:",this.kata)
+        console.log("")
 
-        var arr = this.addIdxFirstLetter(this.kata[0][0])
-        console.log(arr)
-        console.log("kata yang dicari", this.kata[0])
-        //debug
-        // console.log(this.addIdxFirstLetter(this.kata[0][0]))
-        // console.log(arr[0][0] + "-" + arr[0][1] + "-" + this.kata[0].length)
-        // console.log("===="+this.checkGrid(arr[0][0],arr[0][1],this.kata[0].length))
+        let i = 0
+        while(i < banyakKata){
+            // console.log("===TRUE/FALSE=== " + this.solve(this.kata[0]))
+            
+            if(this.solve(this.kata[0]) === true){
+                this.kataAda.push(this.kata[0])
+            }
+            this.kata.shift()
+            i += 1
+            
+            // console.log("")
+            // console.log(`kata-${i} next ${this.kata}`)
+        }
+        return ("kata yang ada di board: " + this.kataAda)
+    }
 
-        // while( arr > 0 || !this.checkGrid(arr[0][0],arr[0][1],this.kata[0].length))
-        if(this.checkGrid(arr[0][0],arr[0][1],this.kata[0].length) === true){
-            console.log("yeay Ada dong")
+    solve(word){
+        var arr = this.addIdxFirstLetter(word[0])
+        //  debug
+        //  console.log("firstLetter location:",arr)
+
+        if(arr.length === 0){
+            // debug
+            // console.log("---kata "+ word +" tidak ada---")
+            return false
         } else {
-            console.log("hmm coba cari lagi deh")
+            // debug
+            // console.log("")
+            // console.log("kamusNow:", this.kata)
+            // console.log("kata yang dicari", word)
+            // console.log("panjang kata:",word.length)
+            // console.log("firstLetter location:",arr)
+            // console.log("init:", [arr[0][0],arr[0][1],word.length])
+
+            while(!this.checkGrid(arr[0][0],arr[0][1],(word.length))){
+                this.deleteidxFirstLetter(arr)
+                // console.log("after delete:", arr)
+                if(arr.length <= 0){
+                    break
+                }
+            }
+            if(arr.length <= 0){
+                return false
+            } else {
+                return true
+            }
         }
 
     }
@@ -69,49 +101,59 @@ class Boggle {
 
     deleteidxFirstLetter(arr){
         arr.shift()
+        // debug
+        // console.log("FirstLetterLocation after shift:",arr)
     }
 
     checkGrid(idxX,idxY,count){
-        if(count === 1){
+        if(count === 0){
             return true
         } else {
             let startX = idxX - 1
             let endX = startX + 2
-            // debug 
-            // console.log("STARTX"+startX+""+endX)
             let startY = idxY - 1
             let endY = startY + 2
-            // debug 
-            // console.log("STARTY"+startY+""+endY)
+
             if(startX < 0){
                 startX = 0
             } else if( endX > this.dimensi-1){
-                endX = this.dimensi
+                endX = this.dimensi-1
             } else if( startY < 0){
                 startY = 0
             } else if(endY > this.dimensi-1){
-                endY = this.dimensi
+                endY = this.dimensi-1
             }
             
+            // debug 
+            // console.log("STARTX: "+startX+"-"+endX)
+            // console.log("STARTY: "+startY+"-"+endY)
+
             for( let i = startX; i <= endX; i++){
-                for(let j = startY; j < endY; j++){
+                for(let j = startY; j <= endY; j++){
                     //debug
                     // console.log("===",this.kata[0][this.kata[0].length - count])
+                    // console.log("huruf dicari:",this.kata[0][this.kata[0].length - count])
                     if(this.kata[0][this.kata[0].length - count] === this.board[i][j]){
                         // debug
-                        // console.log("SAMA==="+i+"-"+j+" count"+count)
+                        // console.log("update:",[i,j,count-1])
+                        // console.log("arrKataAdaNow:", this.kataAda)
+                        // console.log("")
                         return this.checkGrid(i,j,count-1)
                     }
                 }
             }
-
             return false
         }
         
     }
 
 }
+// var kamus = ['TURN']
+// var kamus = ["APPLE"]
+var kamus = ['SIT','APPLE','SUPER','TRIP','TURN','PURE', 'SHIP']
+var banyakKata = kamus.slice(0).length
 
-var hurufBoard = 'DGHIKLPSYEUTTORN'
+var hurufBoard = 'TGHIKLPSYEUTTORN'
+
 var boggle = new Boggle(4)
-boggle.solve()
+console.log(boggle.cekKamus())
