1641f17978eaed1df3a1bc5b817ab562d63ac50c
taqiabdulaziz <svx.annexiv@gmail.com>
2018-11-04 02:37:37 +0700

    BOGGLE SOLVED!

--- boggle.js
+++ boggle.js
@@ -1,13 +1,14 @@
 /*eslint-disable*/
-class Boggle{
-    constructor(num,input) {
-        this.kocoq = this.shake()
+class Boggle {
+    constructor(num, input) {
         this.num = num
         this.input = input
     }
 
     shake() {
-        let arr = []
+        let result = []
+        let arr =[]
+        let moves = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
         let arrTemp = []
         let alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
@@ -22,17 +23,56 @@ class Boggle{
             arr.push(arrTemp)
             arrTemp = []
         }
-        return arr
+        function searchNodes(word, r, c) {
+            
+            if (word.length <= 1) return true;
+            arr[r][c] = '*'
+            let result = false
+
+            //BACKTRACK
+            for (let i = 0; i < moves.length; ++i) {
+                let nextr = r + moves[i][0]
+                let nextc = c + moves[i][1]
+                
+                if (nextr >= 0 && nextr < arr.length &&
+                    nextc >= 0 && nextc < arr[nextr].length &&
+                    arr[nextr][nextc] == word[1] &&
+                    searchNodes(word.slice(1), nextr, nextc)) {
+                    result = true
+                    break
+                }
+            }
+
+            //BACKTRACK RESTORE
+            arr[r][c] = word[0]
+            return result
+        }
+
+        function solve(word) {
+            
+            for (var r = 0; r < arr.length; ++r) {
+                for (var c = 0; c < arr[r].length; ++c) {
+                    // if (arr[r][c] == word[0] && searchNodes(word, r, c)) return true;
+                    if (arr[r][c] == word[0] && searchNodes(word, r, c)) return true;
+                }
+            }
+            return false
+        }
+
+        result = input.filter(
+            function (word) {
+                return solve(word)
+            })
+        console.log(arr);
+        console.log(`${result.length} Word Found!`);
+        result.forEach(element => {
+            console.log(element);
+            
+        });
     }
 }
-let num = 8
-let input = require('./data.js')
-let game = new Boggle(num,input)
-console.log(game.shake());
 
-
-
-
-
-
-
+let num = 4
+let input = require("./data.js")
+let game = new Boggle(num, input)
+game.shake()
