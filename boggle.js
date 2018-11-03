6af3d7294ff0f279e1a144bdd0411e186a62e766
Arnold Herod <arnold.herod@gmail.com>
2018-11-03 13:55:29 +0700

    updated to OOP

--- boggle.js
+++ boggle.js
@@ -1,14 +1,38 @@
-function createBoard(grid){
-    let res = []
-    let arr = []
-    const abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    for(let arrRow = 0 ; arrRow < grid ; arrRow++){
-        res.push(arr)
-        for(let arrCol = 0; arrCol < grid ; arrCol++){
-            let randAbc = abc.charAt(Math.floor(Math.random() * abc.length))
-            res[arrRow].push(randAbc)
-        }
-        arr = []
+class BoggleBoard{
+    constructor(grid){
+        this.diction =
+        [
+            "SIP",
+            "MELON",
+            "PUSH",
+            "PUTS",
+            "KEY"
+        ]
+    
+        this.dummyBoard = 
+        [
+            ['D','G','H','I'],
+            ['K','L','P','S'],
+            ['Y','E','U','T'],
+            ['E','O','R','N']
+        ] 
+
     }
-    return res
+
+    shake(grid){
+        let res = []
+        let arr = []
+        const abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for(let arrRow = 0 ; arrRow < grid ; arrRow++){
+            res.push(arr)
+            for(let arrCol = 0; arrCol < grid ; arrCol++){
+                let randAbc = abc.charAt(Math.floor(Math.random() * abc.length))
+                res[arrRow].push(randAbc)
+            }
+            arr = []
+        }
+        return res
+    }
 }
+const newGame = new BoggleBoard()
+console.log(newGame.shake(4))
