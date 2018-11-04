49683aa4df10b0e62077260f94e508ab1c69b065
unknown <arifinrobert2013@gmail.com>
2018-11-04 20:57:06 +0700

    solve task

--- boggle.js
+++ boggle.js
@@ -1,10 +1,8 @@
 class Boggle {
-    constructor(size, dictionary, wordFound) {
+    constructor(size, searchWord) {
         this.size = size
-        this.dictionary = dictionary
         this.board = []
-        this.searchWord = 'rtor'
-        this.saves = []
+        this.searchWord = searchWord
         this.charFound = []
         this.createBoard()
     }
@@ -23,89 +21,116 @@ class Boggle {
         // }
 
         this.board = [
-            ['o', 'o', 'b', 'c'],
-            ['o', 't', 'o', 't'],
-            ['r', 'r', 'r', 'i'],
-            ['j', 'k', 'm', 'y'],
+            ['S', 'R', 'T', 'M'],
+            ['R', 'A', 'A', 'L'],
+            ['R', 'S', 'S', 'I'],
+            ['A', 'O', 'A', 'T'],
         ];
     }
 
     solve() {
         let wordPos = 0
-        let wordFound = ['rtor']
-        let charFound = []
         let obj = {}
         let d = {}
+        let foundAlphabet = false
+        let foundWords = []
 
-        while (wordPos < wordFound.length) {
+        while (wordPos < this.searchWord.length) {
+            let useBoard = JSON.parse(JSON.stringify(this.board))
+            console.log(useBoard)
+            
             for (let i = 0; i < this.searchWord[wordPos].length; i++) {
-                for ( let y = 0; y < this.board.length; y++) {
-                    for ( let x = 0; x < this.board[y].length; x++) {
-                            if (this.board[y][x] === this.searchWord.charAt(i) && charFound.length === 0) {
-                                console.log('masuk', "isi pertama kali")
-                                console.log('row', y)
-                                console.log('column', x)
+                for (let y = 0; y < useBoard.length; y++) {
+                    for (let x = 0; x < useBoard[y].length; x++) {
+                       if (useBoard[y][x] === this.searchWord[wordPos].charAt(i) && this.charFound.length === 0) {                       
+                            obj.row = y
+                            obj.column = x
+                            obj.value = useBoard[y][x]
+                            useBoard[y][x] = "temp"
+                            this.charFound.push(obj)                
+                           
+                            y = 0
+                            x = 0
+
+                            foundAlphabet = true
+                            break
+                        } else if (useBoard[y][x] === this.searchWord[wordPos].charAt(i)) {
+                            if ((
+                                this.checkTop(y, x) ||
+                                this.checkNorthEast(y , x, useBoard) ||
+                                this.checkRight(y , x, useBoard) ||
+                                this.checkSouthEast(y , x, useBoard) ||
+                                this.checkDown(y ,x, useBoard) ||
+                                this.checkSouthwest(y, x, useBoard) ||
+                                this.checkLeft(y, x) ||
+                                this.checkNorthWest(y, x)
+                            )) {
                                 obj.row = y
                                 obj.column = x
-                                obj.value = this.board[y][x]
-                                charFound.push(obj)
+                                obj.charPos = i
+                                obj.value = useBoard[y][x]
+                                useBoard[y][x] = "temp"
+                                this.charFound.push(obj)
 
-                                this.board[y][x] = 'visited'
                                 y = 0
                                 x = 0
-                            } else if (this.board[y][x] === this.searchWord.charAt(i)) {
-                                if ((
-                                    this.checkTop(y, x) ||
-                                    this.checkNorthEast(y , x) ||
-                                    this.checkRight(y , x) ||
-                                    this.checkSouthEast(y , x) ||
-                                    this.checkSouth(y ,x) ||
-                                    this.checkSouthwest(y, x) ||
-                                    this.checkLeft(y, x) ||
-                                    this.checkNorthWest(y, x)
-                                )) {
-                                    console.log('masuk dan ada isi di array')
-                                    obj.row = y
-                                    obj.column = x
-                                    obj.value = this.board[y][x]
-                                    charFound.push(obj)
 
-                                    this.board[y][x] = 'visited'
-                                    y = 0
-                                    x = 0
-                                } else {
-                                    charFound.pop()
+                                foundAlphabet = true
+                                break
+                            } else {
+                                useBoard[this.charFound[this.charFound.length - 1].row][this.charFound[this.charFound.length - 1].column] = 'visited'                               
+                                this.charFound.pop()
+                                console.log(this.charFound)
+                               
+                          
+                                y = 0
+                                x = 0
+
+                                if (y != this.board.length -1 && x != this.board.length - 1 && this.charFound.length != 0) {
+                                    i = this.charFound[this.charFound.length - 1].charPos
+                                    break
                                 }
                             }
                         }
                     }
-                }
 
-            if (charFound.length == wordFound[wordPos].length) {
-                console.log('word: ' + wordFound[wordPos] + "ditemukan")
-            } else {
-                console.log('word: ' + wordFound[wordPos] + " tidak ditemukan" )
+                    if (foundAlphabet) {
+                        foundAlphabet = false
+                        break
+                    }
+                }
+            }
+
+            if (this.charFound.length == this.searchWord[wordPos].length) {
+              foundWords.push(this.searchWord[wordPos])
             }
 
             wordPos++
         }
+
+        console.log(foundWords.length + " FOUND: ")
+
+        for (let i = 0; i < foundWords.length; i++) {
+            console.log(foundWords[i])
+        }
     }
 
     /**
-     * To  check left side hwnt it is possible
+     * To  check left side if it is possible
      */
     checkLeft(row, column) {
         // 0 0
-        if (this.charFound[length - 1].column - 1 == column && row == this.charFound[length - 1].row) {
+        if (this.charFound[this.charFound.length - 1].column - 1 >= 0 && this.charFound[this.charFound.length - 1].column - 1 == column 
+            && row == this.charFound[this.charFound.length - 1].row) {
             return true
         } else {
             return false
         }
     }
 
-    checkRight(row, column) {
-        if (row == this.charFound[this.charFound.length - 1].row &&
-            this.charFound[this.charFound.length].column + 1 == column) {
+    checkRight(row, column, useBoard) {
+        if (this.charFound[this.charFound.length - 1].column + 1 < useBoard.length && row == this.charFound[this.charFound.length - 1].row &&
+            this.charFound[this.charFound.length - 1].column + 1 === column) {
             return true
         } else {
             return false
@@ -113,33 +138,7 @@ class Boggle {
     }
 
     checkTop(row, column) {
-        if (column == this.charFound[this.charFound.length - 1].column &&
-            this.charFound[this.charFound.length - 1].row -1 == row) {
-            return true
-        } else {
-            return false
-        }
-    }
-
-    checkDown(row, column) {
-        if (column == this.charFound[this.charFound.length - 1].column &&
-            this.charFound[this.charFound.length - 1].row + 1 == row) {
-            return true
-        } else {
-            return false
-        }
-    }
-
-    checkNorthWest(row, column) {
-        if (this.charFound[this.charFound.length - 1].column - 1  === column && this.charFound[this.charFound.length - 1].row - 1 === row) {
-            return true
-        } else {
-            return false
-        }
-    }
-
-    checkNorthEast(row, column) {
-        if (this.charFound[this.charFound.length - 1].column  + 1  === column &&
+        if (this.charFound[this.charFound.length - 1].row - 1 >= 0 && column == this.charFound[this.charFound.length - 1].column &&
             this.charFound[this.charFound.length - 1].row - 1 === row) {
             return true
         } else {
@@ -147,18 +146,55 @@ class Boggle {
         }
     }
 
-    checkSouthEast(row, column) {
-        if (this.charFound[this.charFound.length - 1].column  + 1  === column &&
-            this.charFound[this.charFound.length - 1].row + 1 === row) {
+    checkDown(row, column, useBoard) {
+        if (this.charFound[this.charFound.length - 1].row  + 1 < useBoard.length
+            && column == this.charFound[this.charFound.length - 1].column
+            && this.charFound[this.charFound.length - 1].row + 1 == row) {
             return true
         } else {
             return false
         }
     }
 
-    checkSouthwest(row, column) {
-        if (this.charFound[this.charFound.length - 1].column - 1 === column &&
-            this.charFound[this.charFound.length - 1].row + 1 === row) {
+    checkNorthWest(row, column) {
+        if (this.charFound[this.charFound.length - 1].column - 1 >= 0 
+            && this.charFound[this.charFound.length - 1].row - 1 >= 0
+            && this.charFound[this.charFound.length - 1].column - 1  === column
+            && this.charFound[this.charFound.length - 1].row - 1 === row) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    checkNorthEast(row, column, useBoard) {
+        if (this.charFound[this.charFound.length - 1].column  + 1  < useBoard.length
+            && this.charFound[this.charFound.length - 1].row - 1  >= 0
+            && this.charFound[this.charFound.length - 1].column  + 1 < useBoard.length
+            && this.charFound[this.charFound.length - 1].column  + 1  === column
+            && this.charFound[this.charFound.length - 1].row - 1 === row) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    checkSouthEast(row, column, useBoard) {
+        if (this.charFound[this.charFound.length - 1].column  + 1 < useBoard.length
+            && this.charFound[this.charFound.length - 1].row + 1 < useBoard.length 
+            && this.charFound[this.charFound.length - 1].column  + 1  === column
+            && this.charFound[this.charFound.length - 1].row + 1 === row) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    checkSouthwest(row, column, useBoard) {
+        if (this.charFound[this.charFound.length - 1].column - 1 >= 0
+            && this.charFound[this.charFound.length - 1].row + 1 < useBoard.length 
+            && this.charFound[this.charFound.length - 1].column - 1 === column
+            && this.charFound[this.charFound.length - 1].row + 1 === row) {
             return true
         } else {
             return false
@@ -175,14 +211,5 @@ class Boggle {
     }
 }
 
-const boggle = new Boggle(3)
-boggle.solve();
-
-// O T A
-
-// x   0 1 2
-//
-// y
-// 0   E A B
-// 1   C G T
-// 2   M O A
+const boggle = new Boggle(4,["SAST"])
+boggle.solve()
