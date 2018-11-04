4bb451c4b1302a2e20a83cdf3da7d9d53fcfdc6c
abednegolubis <lubisabednego@gmail.com>
2018-11-04 17:05:17 +0700

    cek koordinat, semua arah mata angin

--- boggle.js
+++ boggle.js
@@ -43,6 +43,65 @@ class Boggle {
     return console.log(printBoard);
   }
 
+  //Release 1
+  // Check direction and check if string is match
+    checkCoordinate(coordinate, direction,boardSize) {
+      let newRow = coordinate[0];
+      let newColumn = coordinate[1];
+      switch (direction) {
+        case 1:
+          newColumn += 1;
+          break;
+        case 2:
+          newRow += 1;
+          newColumn += 1;
+          break;
+        case 3:
+          newRow += 1;
+          break;
+        case 4:
+          newRow += 1;
+          newColumn -= 1;
+          break;
+        case 5:
+          newColumn -= 1;
+          break;
+        case 6:
+          newRow -= 1;
+          newColumn -= 1;
+          break;
+        case 7:
+          newRow -= 1;
+          break;
+        case 8:
+          newRow -= 1;
+          newColumn += 1;
+          break;            
+        default:
+          break;
+      }
+      let newCoordinate = [newRow,newColumn];
+      if (newRow < 0 || newRow >= boardSize || newColumn < 0 || newColumn >= boardSize || this.checkIncludes(newCoordinate, this.usedCoordinate) === true) {
+        return false;
+      } else {
+        return newCoordinate;
+      }
+    }
+  
+    checkIncludes(coordinate, arrayUsedCoordinate) {
+      let strCoor = ""+coordinate[0]+coordinate[1];
+      for (let i = 0 ; i < arrayUsedCoordinate.length; i++) {
+        let strArrUsedCoor = "";
+        for (let j = 0 ; j < arrayUsedCoordinate[i].length ; j++) {
+          strArrUsedCoor += arrayUsedCoordinate[i][j];
+        }
+        if (strArrUsedCoor === strCoor) {
+          return true;
+        }
+      }
+      return false;
+    }
+
 }
 
 var game = new Boggle();
@@ -51,4 +110,18 @@ var game = new Boggle();
 let shuffledBoard = (game.shake(4));
 console.log("RELESE 0 TEST")
 game.printBoard(shuffledBoard);
+console.log("")
+
+let sampleBoard = [
+  ['D', 'G', 'H', 'I'],
+  ['K', 'L', 'P', 'S'],
+  ['Y', 'E', 'U', 'T'],
+  ['E', 'O', 'R', 'N']
+]
+console.log("RELESE 1 TEST")
+game.printBoard(sampleBoard);
+console.log("TURN", game.findString("TURN",sampleBoard));
+console.log("SUPER",game.findString("SUPER",sampleBoard));
+console.log("APPLE",game.findString("APPLE",sampleBoard));
+console.log("PUPI",game.findString("PUPI",sampleBoard));
 console.log("")
