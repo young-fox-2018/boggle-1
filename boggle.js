44563362a053c1bbfde02baf6c8db3688c961b69
Zhang <zhang@ZhangMBP15.local>
2018-11-04 10:31:51 +0700

    tinggal solve

--- boggle.js
+++ boggle.js
@@ -1,37 +1,78 @@
 class Boggle {
-  constructor(){
-    this.papan = this.shake()
+  constructor(size){
+    this.papan = []
+    this.kamus = kamus
     this.dummy = [ [ 'D', 'G', 'H', 'I' ],
     [ 'K', 'L', 'P', 'S' ],
     [ 'Y', 'E', 'U', 'T' ],
     [ 'E', 'O', 'R', 'N' ] ]
-    this.kamus = [  'APPLE' , 'SIT' , 'TRIP' , 'TURN' , 'SUPER']
+    this.kamustest = [  'APPLE' , 'SIT' , 'TRIP' , 'TURN' , 'SUPER']
+    this.size = size
   }
   // BUAT NAMPILIN BOARDNYA
   shake() {
-    var result = []
-    for(var i = 1 ; i < 5 ; i++){
+    for(var i = 1 ; i <= this.size ; i++){
       var arr = []
-      for(var j = 1 ; j < 5; j++){
+      for(var j = 1 ; j <= this.size; j++){
         arr.push(this.randomAbjad())
       }
-      result.push(arr)
+      this.papan.push(arr)
     }
-    console.log(result)
+    return this.papan
   }
+
   // BUAT NGERANDOM ABZAD
   randomAbjad() {
     var text = '';
     var abjad = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  
     text += abjad.charAt(Math.floor(Math.random() * abjad.length))
     return text
   }
 
+  cekPertama (board , kamus){
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        if (board[i][j] == kamus ) {
+          return [true , [i,j]]
+        }        
+      }      
+    }
+    return [false]
+  }
+
+  cekGrid(board, I,J,kamus) {
+    let istart = I-1
+    let iend = I+1
+    let jstart = J-1
+    let jend = J+1
+
+   if (istart < 0 ) {
+      istart = 0 
+   } else if (iend > this.size - 1) {
+      iend = this.size -1
+   } else if (jstart < 0 ) {
+      jstart = 0
+   } else if ( jend > this.size - 1 ) {
+      jend  = this.size - 1
+   }
+
+    for (let i = xstart; i <= xend; i++) {
+      for (let j = ystart; j <= yend; j++) {
+        if (board[i][j] == kamus){
+          return [true, [i,j]]
+        }
+      } 
+    } 
+  return [false] 
+  }
+
 
 }
-
-var board = new Boggle()
+const kamus = require('./data.js')
+var board = new Boggle(4)
 // manggil papannya udah bisa
+// console.log(board.shake())
 // manggil kamus
-board.papan
+// console.log(board.kamustest)
+// manggil dummy papan
+// console.log(board.dummy)
