7a445279acefa17f556cd095ea6a41980302c5a6
DzikryFathullah <dzizee.fathullah@gmail.com>
2018-11-04 20:41:56 +0700

    backtrack update

--- boggle.js
+++ boggle.js
@@ -1,7 +1,7 @@
 class Boggle {
     constructor(){
         this.board = []
-        this.history = []
+        this.words = ['TURN']
     }
 
     randomAlphabeth() {
@@ -19,7 +19,8 @@ class Boggle {
     }
 
     dummyShake() {
-        const dummyAlph = 'DGHIKLPSYEUTEORN'
+        const dummyAlph = 'TGTTKULUYZURNORN'
+        // const dummyAlph = 'DGHIKLPSYEUTEORN'
         let index = 0
         for (let i=0; i < 4; i++) {
             this.board.push([])
@@ -28,9 +29,79 @@ class Boggle {
             }
         }
     }
+
+    solve() {
+        let wordFound = 0
+        for (let i = 0; i < this.words.length; i++) {
+            let histories = []
+            let wordBoard = JSON.parse(JSON.stringify(this.board))
+            for (let j = 0; j < this.words[i].length; j++) {
+                if(j === 0) {
+                    if (this.findFirstAlph(wordBoard,this.words[i][j]) === 'blank') break;
+                    else {
+                        histories.push(this.findFirstAlph(wordBoard,this.words[i][j]))
+                        wordBoard[histories[j].x][histories[j].y] = ' '
+                    }
+                } else {
+                    if(this.findAlph(wordBoard,this.words[i][j],histories[j-1]) === 'blank') {
+                        j-=2
+                        histories.pop()
+                    } else {
+                        histories.push(this.findAlph(wordBoard,this.words[i][j],histories[j-1]))
+                        wordBoard[histories[j].x][histories[j].y] = ' '
+                    }
+                }
+            }
+            console.log(wordBoard)
+            console.log(histories)
+        }
+    }
+    
+    findFirstAlph(board,str) {
+        let status = false
+        let pos = {}
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                if (str === board[i][j]){
+                    pos.alphabeth = board[i][j]
+                    pos.x = i
+                    pos.y = j
+                    status = true
+                } 
+                if (status === true) return pos
+                else if (status === false && (i === board.length-1 && j === board.length-1)) return 'blank'
+            }
+        }
+    }
+    
+    findAlph(board,str,history) {
+        let pos = {}
+        let status = false
+        let maxi = history.x + 1
+        let mini = history.x - 1
+        let maxj = history.y + 1
+        let minj = history.y - 1
+        if (maxi > 3) maxi = 3
+        if (maxj > 3) maxj = 3
+        if (mini < 0) mini = 0
+        if (minj < 0) minj = 0
+        for(let i = mini; i <= maxi; i++) {
+            for (let j = minj; j <= maxj; j++) {
+                if (board[i][j] === str) {
+                    pos.alphabeth = board[i][j]
+                    pos.x = i
+                    pos.y = j
+                    status = true
+                }
+                if (status === false && (i === maxi && j === maxj)) return 'blank'
+                if (status === true) return pos
+            }
+        }
+    }
 }
 
 let main = new Boggle()
 
 main.dummyShake()
-console.log(main.board)
+// console.log(main.board)
+main.solve()
